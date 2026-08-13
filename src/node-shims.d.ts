6522454a2bb6188ea96c001b893ffd851849f2bc
Senad  Dizdarević <112484166+senad-d@users.noreply.github.com>
2026-08-13 12:55:11 +0200

    1

--- src/node-shims.d.ts
+++
@@ -1,9 +0,0 @@
-declare const process: { cwd(): string };
-
-declare module "node:fs" {
-  export function readFileSync(path: string, encoding: "utf8"): string;
-}
-
-declare module "node:crypto" {
-  export const webcrypto: Crypto;
-}
